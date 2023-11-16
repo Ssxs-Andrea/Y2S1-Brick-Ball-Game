5ed5e4cf2ab7930d379ae37acc43f4eab5726c5e
Syn Yee Wong <synyeew@gmail.com>
2023-11-16 23:16:48 +0800

    Instruction Page
    Design the instruction page with images and text.
    Create method getInstructionsBox to get the instructions. Use imageView to set the size of images. Use textFlow to display the series of text.
     Create method createLabelWithImage to pair the text with the image. Add style to the text.
    
    Create instruction.css to style the elements.
    Put images and css in instruction folder in resources.

--- src/main/java/brickGame/InstructionPage.java
+++ src/main/java/brickGame/InstructionPage.java
@@ -1,11 +1,15 @@
 package brickGame;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class InstructionPage {
     private Scene scene; // Reference to the InstructionPage Scene
@@ -17,43 +21,90 @@ public class InstructionPage {
     }
 
     private void init() {
-        Text instructionsText = getInstructionsText();
+        VBox instructionsBox = getInstructionsBox();
 
         Button backButton = new Button("Back To Main Menu");
-        backButton.setOnAction(e -> main.restartGame());
-        backButton.setTranslateX(180);
-        backButton.setTranslateY(380);
-        backButton.setPrefSize(150,30);
+        backButton.setOnAction(e -> main.switchToMainMenuPage());
+        backButton.setTranslateX(70);
 
-        Pane pane = new Pane(instructionsText, backButton);
-
-        scene = new Scene(pane, 500, 700);
-        scene.setOnKeyPressed(this::handle);
+        VBox vbox = new VBox(instructionsBox, backButton);
+        scene = new Scene(vbox, 500, 700);
+        scene.getStylesheets().add(getClass().getResource("/instruction/instruction.css").toExternalForm());
     }
+    private VBox getInstructionsBox() {
+        Image leftKeyImage = new Image(getClass().getResource("/instruction/arrow-left.png").toExternalForm());
+        Image rightKeyImage = new Image(getClass().getResource("/instruction/arrow-right.png").toExternalForm());
+        Image mouseImage = new Image(getClass().getResource("/instruction/click.png").toExternalForm());
+        Image rKeyImage = new Image(getClass().getResource("/instruction/letter-r.png").toExternalForm());
+        Image pKeyImage = new Image(getClass().getResource("/instruction/letter-p.png").toExternalForm());
+        Image sKeyImage = new Image(getClass().getResource("/instruction/letter-s.png").toExternalForm());
+        Image lKeyImage = new Image(getClass().getResource("/instruction/letter-l.png").toExternalForm());
 
-    private static Text getInstructionsText() {
-        Text instructionsText = new Text("Instructions:\n\n" +
-                "Press 'LEFT' and 'RIGHT' to move the paddle\n" +
-                "Use mouse to drag and move the paddle\n" +
-                "Press 'R' to restart the game\n" +
-                "Press 'P' to pause and resume the game\n" +
-                "Press 'S' to save the game\n" +
-                "Press 'L' to load a saved game");
-        instructionsText.setFont(new Font(10));
-        instructionsText.setTranslateX(20);
-        instructionsText.setTranslateY(50);
-        return instructionsText;
+        ImageView leftKeyImageView = new ImageView(leftKeyImage);
+        ImageView rightKeyImageView = new ImageView(rightKeyImage);
+        ImageView mouseImageView = new ImageView(mouseImage);
+        ImageView rKeyImageView = new ImageView(rKeyImage);
+        ImageView pKeyImageView = new ImageView(pKeyImage);
+        ImageView sKeyImageView = new ImageView(sKeyImage);
+        ImageView lKeyImageView = new ImageView(lKeyImage);
+
+        leftKeyImageView.setFitWidth(40);
+        leftKeyImageView.setFitHeight(40);
+
+        rightKeyImageView.setFitWidth(40);
+        rightKeyImageView.setFitHeight(40);
+
+        mouseImageView.setFitWidth(40);
+        mouseImageView.setFitHeight(40);
+
+        rKeyImageView.setFitWidth(40);
+        rKeyImageView.setFitHeight(40);
+
+        pKeyImageView.setFitWidth(40);
+        pKeyImageView.setFitHeight(40);
+
+        sKeyImageView.setFitWidth(40);
+        sKeyImageView.setFitHeight(40);
+
+        lKeyImageView.setFitWidth(40);
+        lKeyImageView.setFitHeight(40);
+
+        Text instructionsText = new Text("Instructions:\n\n");
+        instructionsText.setId("instructionsText");
+        instructionsText.getStyleClass().add("instructions-header");
+
+        TextFlow textFlow = new TextFlow(
+                instructionsText,
+                createLabelWithImage(leftKeyImageView, "   Move Paddle Left\n"),
+                createLabelWithImage(rightKeyImageView, "   Move Paddle Right\n"),
+                createLabelWithImage(mouseImageView, "   Drag and Move Paddle\n"),
+                createLabelWithImage(rKeyImageView, "   Restart the Game\n"),
+                createLabelWithImage(pKeyImageView, "   Pause the Game\n"),
+                createLabelWithImage(sKeyImageView, "   Save the Game\n"),
+                createLabelWithImage(lKeyImageView, "   Load the Game\n")
+        );
+
+        VBox instructionsBox = new VBox(textFlow);
+        instructionsBox.setPadding(new Insets(40));
+
+        return instructionsBox;
+    }
+    private Label createLabelWithImage(ImageView imageView, String text) {
+        Label label = new Label();
+        label.setGraphic(imageView);
+        label.setContentDisplay(ContentDisplay.LEFT);
+        label.setPadding(new Insets(0, 0, 10, 10));
+        label.setGraphicTextGap(3);
+        label.setWrapText(true);
+
+        label.getStyleClass().add("text-ins");
+        label.setWrapText(true);
+        label.setText(text);
+
+        return label;
     }
 
     public Scene getScene() {
         return scene;
-    }
-
-    public void handle(KeyEvent event) {
-        switch (event.getCode()) {
-            case R:
-                main.restartGame();
-                break;
-        }
     }
 }
